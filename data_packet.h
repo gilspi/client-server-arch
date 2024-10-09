157a85a42c09208fa5e77a53370607148023a69e
gilspi <pashabicepsv_p@mail.ru>
2024-10-09 12:35:59 +0300

    change structure of the packet

--- data_packet.h
+++ data_packet.h
@@ -1,11 +1,24 @@
 #ifndef DATA_PACKET_H
 #define DATA_PACKET_H
 
+#include <stdint.h>
+
 struct DataPacket {
-    int id;
-    float value;
-    char msg[50];
+    struct Header {
+        uint32_t id;
+        double version;
+        char description[100];
+    } header;
+
+    struct Data {
+        int numbers[50];
+        char strings[5][50];
+    } data;
+
+    uint8_t reserved[64];
 };
+
+void generateRandomData(struct DataPacket* packet);
 
 #endif // DATA_PACKET_H
 
